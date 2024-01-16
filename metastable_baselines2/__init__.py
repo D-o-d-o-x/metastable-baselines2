d5e53a87db0a0ac84388d97c1f4e6eafd610a656
Dominik Roth <dominik.roth.dev@gmail.com>
2024-01-16 15:34:12 +0100

    Expose TRPL class

--- metastable_baselines2/__init__.py
+++ metastable_baselines2/__init__.py
@@ -1,7 +1,9 @@
 from metastable_baselines2.ppo import PPO
 from metastable_baselines2.sac import SAC
+from metastable_baselines2.trpl import TRPL
 
 __all__ = [
     "PPO",
     "SAC",
+    "TRPL",
 ]
