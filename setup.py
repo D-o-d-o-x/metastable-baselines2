3fa8beb7ae10aa277359e1cbc746b1ac253d456e
Dominik Roth <dominik.roth.dev@gmail.com>
2024-04-01 00:17:50 +0200

    Make pca optional dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,15 @@ setup(
     url='https://git.dominik-roth.eu/dodox/metastable-baselines2',
     author='Dominik Roth',
     author_email='mail@dominik-roth.eu',
-    # description='Description of my package',
-    packages=['.'],
-    install_requires=['gymnasium', 'stable_baselines3==2.1.0', '-e git+ssh://git@dominik-roth.eu/dodox/PriorConditionedAnnealing.git#egg=priorConditionedAnnealing'],
+    packages=find_packages(),
+    install_requires=[
+        'gymnasium',
+        'stable_baselines3==2.1.0',
+    ],
+    extras_require={
+        'pca': [
+            'priorConditionedAnnealing @ git+ssh://git@dominik-roth.eu/dodox/PriorConditionedAnnealing.git#egg=priorConditionedAnnealing'
+        ],
+    },
 )
+
